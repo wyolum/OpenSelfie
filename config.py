e7f043cf7e0863ec71928aa8662345f6690ed2a3
= <=>
2014-03-22 12:18:05 -0400

    re-orged into standardized directories

--- config.py
+++
@@ -1,6 +0,0 @@
-#set these up in your google account
-global username,password, gmailUser, gmailPassword
-username = 'XXXXXXX@gmail.com'
-password = 'YYYYYYYY'
-gmailUser = 'ZZZZ@gmail.com'
-gmailPassword = 'QQQQQQQ'
