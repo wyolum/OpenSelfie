957c4e9cc9fe7b25d5402f625e06964021884166
Kevin Osborn <kevin.osborn@gmail.com>
2014-04-09 15:51:04 -0400

    move the updated files into scrips

---
+++ scripts/custom.py
@@ -0,0 +1,5 @@
+global emailSubject,emailMsg,logopng
+
+emailSubject = "Your Postcard from the Wyolum Photobooth"
+emailMsg = "Here's your picture from the http://wyolum.com photobooth!"
+logopng = "logo.png"
