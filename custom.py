4ce16090b39c8200b857d2fa132cd18b22c50e68
Kevin Osborn <kevin.osborn@gmail.com>
2014-04-09 15:51:34 -0400

    remove custom.py from parent dir

--- custom.py
+++
@@ -1,5 +0,0 @@
-global emailSubject,emailMsg,logopng
-
-emailSubject = "Your Postcard from the Wyolum Photobooth"
-emailMsg = "Here's your picture from the http://wyolum.com photobooth!"
-logopng = "logo.png"
