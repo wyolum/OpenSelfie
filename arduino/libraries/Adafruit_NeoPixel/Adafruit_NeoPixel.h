2dfb74c526ab2906aa37babb3f089a0133d09664
= <=>
2014-05-08 07:51:59 -0400

    chaned to orig neopix lib

--- arduino/libraries/Adafruit_NeoPixel/Adafruit_NeoPixel.h
+++ arduino/libraries/Adafruit_NeoPixel/Adafruit_NeoPixel.h
@@ -15,6 +15,7 @@
   License along with NeoPixel.  If not, see
   <http://www.gnu.org/licenses/>.
   --------------------------------------------------------------------*/
+
 #ifndef ADAFRUIT_NEOPIXEL_H
 #define ADAFRUIT_NEOPIXEL_H
 
@@ -29,53 +30,62 @@
 #define NEO_RGB     0x00 // Wired for RGB data order
 #define NEO_GRB     0x01 // Wired for GRB data order
 #define NEO_COLMASK 0x01
-#define NEO_KHZ400  0x00 // 400 KHz datastream
 #define NEO_KHZ800  0x02 // 800 KHz datastream
 #define NEO_SPDMASK 0x02
-#define BLACK (uint32_t)0x00
+// Trinket flash space is tight, v1 NeoPixels aren't handled by default.
+// Remove the ifndef/endif to add support -- but code will be bigger.
+// Conversely, can comment out the #defines to save space on other MCUs.
+#ifndef __AVR_ATtiny85__
+#define NEO_KHZ400  0x00 // 400 KHz datastream
+#endif
 
 class Adafruit_NeoPixel {
 
  public:
 
   // Constructor: number of LEDs, pin number, LED type
-  Adafruit_NeoPixel();
-  void setup(uint16_t n, uint8_t p=6, uint8_t t=NEO_GRB + NEO_KHZ800);
-  void setup(uint16_t n, uint8_t p, uint8_t t, uint8_t *_pixels);
-  void changePin(uint8_t p);
+  Adafruit_NeoPixel(uint16_t n, uint8_t p=6, uint8_t t=NEO_GRB + NEO_KHZ800);
+  ~Adafruit_NeoPixel();
 
   void
     begin(void),
     show(void),
+    setPin(uint8_t p),
     setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b),
-    setPixelColor(uint16_t n, uint32_t c);
+    setPixelColor(uint16_t n, uint32_t c),
+    setBrightness(uint8_t);
+  uint8_t
+   *getPixels(void) const;
   uint16_t
-    numPixels(void);
+    numPixels(void) const;
+  static uint32_t
+    Color(uint8_t r, uint8_t g, uint8_t b);
   uint32_t
-    Color(uint8_t r, uint8_t g, uint8_t b),
-    getPixelColor(uint16_t n);
-    uint8_t * getRow(void);
-    void setRow(uint8_t *);
-    void clearRow(void);
+    getPixelColor(uint16_t n) const;
 
+ private:
 
-  // moved to public to expose to serial interaction justin@wyolum.com
-  uint8_t     *pixels;        // Holds LED color values (3 bytes each)
-  uint16_t     numBytes;      // Size of 'pixels' buffer below
-  
-  // private:
-  bool changed;
-
-  uint16_t
-    numLEDs;       // Number of RGB LEDs in strip
+  const uint16_t
+    numLEDs,       // Number of RGB LEDs in strip
+    numBytes;      // Size of 'pixels' buffer below
   uint8_t
     pin,           // Output pin number
-    pinMask,       // Output PORT bitmask
+    brightness,
+   *pixels,        // Holds LED color values (3 bytes each)
+    rOffset,       // Index of red byte within each 3-byte pixel
+    gOffset,       // Index of green byte
+    bOffset;       // Index of blue byte
+  const uint8_t
     type;          // Pixel flags (400 vs 800 KHz, RGB vs GRB color)
-  volatile uint8_t
-    *port;         // Output PORT register
   uint32_t
     endTime;       // Latch timing reference
+#ifdef __AVR__
+  const volatile uint8_t
+    *port;         // Output PORT register
+  uint8_t
+    pinMask;       // Output PORT bitmask
+#endif
 
 };
-#endif
+
+#endif // ADAFRUIT_NEOPIXEL_H
