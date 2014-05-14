14a348dbd13fec2c192b9566c63c28a6ae2c3130
Justin Shaw <wyojustin@gmail.com>
2014-05-14 13:19:49 +0000

    added username/password cashing

--- scripts/config.py
+++ scripts/config.py
@@ -1,7 +1,38 @@
 #set these up in your google account
-global username,password, gmailUser, gmailPassword
-username = 'XXXXXXX@gmail.com'
-password = 'YYYYYYYY'
-gmailUser = 'ZZZZ@gmail.com'
-gmailPassword = 'QQQQQQQ'
+import getpass
+import os.path
 
+class Credential:
+    def __init__(self, name='password', hidden=True):
+        self.name = name
+        self.prompt = self.name + ': '
+        if not os.path.exists(self.name):
+            if hidden:
+                self.value = getpass.getpass(self.prompt)
+            else:
+                self.value = raw_input(self.prompt)
+            open(self.name, 'wb').write(self.value)
+        else:
+            self.value = open(self.name).read().strip()
+    def __str__(self):
+        return self.value
+
+    def reset(self):
+        if os.path.exists(self.name):
+            os.unlink(self.name)
+            self.value = open(self.name).read()
+
+def Credential__test__():
+    p = Credential('password')
+    q = Credential('password')
+    assert p == q
+    p.reset()
+        
+# username = 'XXXXXXX@gmail.com'
+# password = 'YYYYYYYY'
+# gmailUser = 'ZZZZ@gmail.com'
+# gmailPassword = 'QQQQQQQ'
+username = Credential('username', hidden=False)
+password = Credential('password')
+gmailUser = username
+gmailPassword = password
