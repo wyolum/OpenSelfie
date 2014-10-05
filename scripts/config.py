c1a891620d0a6f7a7c7f2c3fc58718aab5ba30d1
Justin Shaw <wyojustin@gmail.com>
2014-10-05 17:50:52 -0400

    changed geometry

--- scripts/config.py
+++ scripts/config.py
@@ -3,27 +3,24 @@ import getpass
 import os.path
 
 class Credential:
-    def __init__(self, prompt='Username:', secret=False):
-        self.prompt = prompt
-        self.secret = secret
-        self.filename = '.' + prompt
-        if os.path.exists(prompt):
-            self.value = open(self.filename).read()
-        else:
-            self.value = None
-    def __repr__(self):
-        if self.value is None:
-            if self.secret:
-                self.value = getpass.getpass(self.prompt)
-            else:
-                self.value = raw_input(self.prompt)
-                open(self.filename, 'w').write(self.value)
-        return self.value
-    def __str__(self):
-        return self.__repr__()
+    def __init__(self):
+        self.filename = '.credentials'
 
-username = Credential(prompt="AppSpecificPassword:")
-password = Credential(prompt='Password:', secret=True)
+        if os.path.exists(self.filename):
+            f = open(self.filename)
+            self.key = f.readline().strip()
+            self.value = f.readline().strip()
+        else:
+            self.key = raw_input('Google Username:')
+            self.value = getpass.getpass('App Specific Password')
+            f = open(self.filename, 'w')
+            f.write(self.key + '\n')
+            f.write(self.value + '\n')
+
+
+cred = Credential()
+username = cred.key
+password = cred.value
 gmailUser = username
 gmailPassword = password
 
