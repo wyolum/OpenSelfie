53434ab717162464d2d9084bd453c590bf83ca74
Justin Shaw <wyojustin@gmail.com>
2014-12-09 18:08:27 -0500

    fixed bug in TiM lada... was too fat

--- fabricate/lada_base.py
+++ fabricate/lada_base.py
@@ -4,11 +4,11 @@ from lada import *
 
 ### main enclosure
 T = 3* mm
-main_LENGTH = 269 * mm + 2 * T
-main_WIDTH = 206 * mm + 2 * T
-main_HEIGHT = 56 * mm + 2 * T
+main_LENGTH = 300 * mm
+main_WIDTH = 50.5 * mm
+main_HEIGHT = 210 * mm
 can = new_canvas("NovaBooth_lada.pdf", 20*inch, 12*inch, .5*inch)
-main_lada = Lada(main_LENGTH, main_WIDTH, main_HEIGHT, T, max_edge_span=60*mm)
+main_lada = Lada(main_LENGTH, main_WIDTH, main_HEIGHT, T, max_edge_span=100*mm)
 main_lada.drawOn(can)
 can.save()
 print 'wrote', can._filename
@@ -16,9 +16,9 @@ main_scad = main_lada.toScad(name='main')
 
 ### TiM Case
 T = 3* mm
-tim_LENGTH = 4.2 * inch + 2 * T + 2 * .4 * inch # main_WIDTH + 2 * T
-tim_WIDTH = main_HEIGHT + 2 * T
-tim_HEIGHT =  main_LENGTH + 2 * T
+tim_LENGTH = 5 * inch + 2 * T
+tim_WIDTH = main_LENGTH + 2 * T
+tim_HEIGHT =  main_WIDTH + 2 * T
 can = new_canvas("TiM_lada.pdf", 20*inch, 12*inch, .5*inch)
 tim_lada = Lada(tim_LENGTH, tim_WIDTH, tim_HEIGHT, T, max_edge_span=400*mm)
 tim_lada.drawOn(can)
